d49f4442a1b37fa39d878f6ae3533a1135c4b126
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2023-06-15 12:53:58 -0300

    end point user register created

--- note_api/app.js
+++ note_api/app.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
+// var logger = require('morgan');
 require('./config/database');
 
 var usersRouter = require('./app/routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+console.log('Criado por Lucas Miranda')
 module.exports = app;
--- note_api/config/database.js
+++ note_api/config/database.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongoose://localhost/javascriptNote', {
+mongoose.connect('mongodb://127.0.0.1:27017/javascriptNote', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(() => console.log('Connection succesful'))
-.catch((err) => console.log(err));
+    useUnifiedTopology: true    
+}).then(() => console.log('Connection succesful!'))
+.catch((err) => console.log(`Erro no Banco: ${err}` ));
