afe535b29712b22afefa19707b66b843f30c63b3
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2023-06-24 11:12:37 -0300

    implementation of get notes functionality

--- note_api/app/routes/notes.js
+++ note_api/app/routes/notes.js
@@ -17,5 +17,37 @@ router.post('/', WithAuth, async(req, res)=>{
     }
 })
 
+router.get('/:id', WithAuth, async(req,res)=>{
+    const{id} = req.params;
+    
+    try {
+        const note = await Note.findById(id);
+        if(isOwner(req.user, note)){
+            res.status(200).json(note);
+        }else{
+            res.status(403).json({error : `Permission denied!`});
+        }
+    } catch (error) {
+        res.status(500).json({error : `Problem to get a note ${error}`});
+    }
+})
+
+router.get('/', WithAuth, async(req, res)=>{
+    try {
+        const notes = await Note.find({author: req.user._id});
+        res.status(200).json(notes);
+    } catch (error) {
+        res.status(500).json({error : `Problem to get a notes ${error}`});
+    }
+})
+
+const isOwner = (user, note)=>{   
+    if (JSON.stringify(user._id) === JSON.stringify(note.author)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 
 module.exports = router;
