51dddaba9ea5ea4c7741cb0b7cd9e198eb18a8df
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2023-06-25 20:32:24 -0300

    Added update, delete and search notes functionality

--- note_api/app/routes/notes.js
+++ note_api/app/routes/notes.js
@@ -17,6 +17,22 @@ router.post('/', WithAuth, async(req, res)=>{
     }
 })
 
+router.get('/search', WithAuth, async(req,res)=>{
+    const { query } = req.query;
+    try {
+        let notes = await Note.find({author: req.user._id})
+        .find({ $text: {$search: query}});
+        if(notes.length >= 1 ){
+            res.json(notes);
+        }else{
+            res.status(500).json({message: 'no results found'})
+        }
+        
+    } catch (error) {
+        res.status(500).json({error : `Problem to get a note(s) ${error}`});
+    }
+})
+
 router.get('/:id', WithAuth, async(req,res)=>{
     const{id} = req.params;
     
@@ -40,6 +56,44 @@ router.get('/', WithAuth, async(req, res)=>{
         res.status(500).json({error : `Problem to get a notes ${error}`});
     }
 })
+
+router.put('/:id', WithAuth, async(req, res)=>{
+    const { title, body} = req.body;
+    const {id} = req.params;
+
+    try {
+        let note = await Note.findById(id);
+        if(isOwner(req.user, note)){
+            let note = await Note.findByIdAndUpdate(id,
+                { $set: {title: title , body: body} },
+                { upsert: true , 'new': true }
+                );
+
+                res.json(note);
+        }else{
+            res.status(403).json({error : `Permission denied!`});
+        }
+    } catch (error) {
+        res.status(500).json({error : `Problem to update a note`});
+    }
+})
+
+router.delete('/:id', WithAuth, async(req, res)=>{
+    const {id} = req.params;
+    try {
+        let note = await Note.findById(id);
+        if (isOwner(req.user, note)) {
+            await note.deleteOne();
+            res.status(200).json({message: "deleted note"})
+        } else {
+            res.status(403).json({error : `Permission denied!`});
+        }
+    } catch (error) {
+        res.status(500).json({error : `Problem to delete a note`}); 
+    }
+})
+
+
 
 const isOwner = (user, note)=>{   
     if (JSON.stringify(user._id) === JSON.stringify(note.author)){
